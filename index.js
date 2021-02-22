3d61b57cee0635351b0b78941afbffd15a899d40
zuzzleberry <azuanic@gmail.com>
2021-02-22 16:52:15 +1100

    "Finished logic. Perfected mobile view CSS"

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let slider = document.querySelector("#price-slider");
+let sliderFill = document.querySelector("#price-slider-fill")
 let pageviews = document.querySelector("#pageviews");
 let price = document.querySelector("#price-number");
 
@@ -6,35 +7,67 @@ let toggleButtonCircle = document.querySelector("#toggle-btn-circle");
 let toggleButtonBody = document.querySelector("#toggle-btn-body");
 let buttonClicked = false;
 
-let priceOptions = [
-    ["10K PAGEVIEWS", 8.00],
-    ["50K PAGEVIEWS", 12.00],
-    ["100K PAGEVIEWS", 16.00],
-    ["500K PAGEVIEWS", 24.00],
-    ["1M PAGEVIEWS", 36.00]
+let priceDescriptions = [
+    "10K PAGEVIEWS",
+    "50K PAGEVIEWS",
+    "100K PAGEVIEWS",
+    "500K PAGEVIEWS",
+    "1M PAGEVIEWS",
 ]
+
+let priceOptions = [
+    8.00,
+    12.00,
+    16.00,
+    24.00,
+    36.00
+]
+let discountPrices = [];
+
+let priceRead = () => {
+    if (buttonClicked === false) {
+        return priceOptions;
+    } else {
+        return discountPrices;
+    }
+}
 
 let updateInfo = (value) => {
     if (value > 0 && value <= 20) {
-        pageviews.innerHTML = priceOptions[0][0];
-        price.innerHTML = `$${priceOptions[0][1].toFixed(2).toString()}`;
+        pageviews.innerHTML = priceDescriptions[0];
+        price.innerHTML = `$${priceRead()[0].toFixed(2).toString()}`;
     } else if (value > 20 && value <= 40) {
-        pageviews.innerHTML = priceOptions[1][0];
-        price.innerHTML = `$${priceOptions[1][1].toFixed(2).toString()}`;
+        pageviews.innerHTML = priceDescriptions[1];
+        price.innerHTML = `$${priceRead()[1].toFixed(2).toString()}`;
     } else if (value > 40 && value <= 60) {
-        pageviews.innerHTML = priceOptions[2][0];
-        price.innerHTML = `$${priceOptions[2][1].toFixed(2).toString()}`;
+        pageviews.innerHTML = priceDescriptions[2];
+        price.innerHTML = `$${priceRead()[2].toFixed(2).toString()}`;
     } else if (value > 60 && value <= 80) {
-        pageviews.innerHTML = priceOptions[3][0];
-        price.innerHTML = `$${priceOptions[3][1].toFixed(2).toString()}`;
+        pageviews.innerHTML = priceDescriptions[3];
+        price.innerHTML = `$${priceRead()[3].toFixed(2).toString()}`;
     } else if (value > 80 && value <= 100) {
-        pageviews.innerHTML = priceOptions[4][0];
-        price.innerHTML = `$${priceOptions[4][1].toFixed(2).toString()}`;
+        pageviews.innerHTML = priceDescriptions[4];
+        price.innerHTML = `$${priceRead()[4].toFixed(2).toString()}`;
     }
+    sliderFillUpdate();
 };
+
+let discount = () => {
+    for (i = 0; i < priceOptions.length; i++) {
+        discountPrices[i] = priceOptions[i] - (priceOptions[i] * .25)
+    }
+}
+
+sliderFillUpdate = () => {
+    sliderFill.style.width = `${slider.value}%`
+}
 
 let slideValueChange = (e) => {
     slider.addEventListener("mousemove", (e) => {
+        let value = e.target.value;
+        updateInfo(value);
+    });
+    slider.addEventListener("touchmove", (e) => {
         let value = e.target.value;
         updateInfo(value);
     });
@@ -45,25 +78,28 @@ toggleButtonBody.addEventListener("click", () => {
         toggleButtonBody.style.backgroundColor = "hsl(174, 86%, 45%)";
         toggleButtonCircle.style.marginLeft = "1.1rem";
         buttonClicked = true;
-
-        // for (i = 0; i < priceOptions.length; i++) {
-        //     priceOptions[i][1] = priceOptions[i][1] - (priceOptions[i][1] * .25)
-        // }
-        // updateInfo(slider);
-        
-
+        discount();
+        updateInfo(slider.value);
     } else if (buttonClicked === true) {
         toggleButtonBody.style.backgroundColor = "hsl(223, 50%, 87%)";
         toggleButtonCircle.style.marginLeft = "0rem";
         buttonClicked = false;
+        updateInfo(slider.value);
     }
 })
 
 slider.addEventListener("mousedown", (e) => {
-    slideValueChange(e);
+    slideValueChange(e); 
 });
-slider.addEventListener("touchmove", (e) => {
+slider.addEventListener("mouseup", (e) => {
+    sliderFillUpdate();
+});
+
+slider.addEventListener("touchstart", (e) => {
     slideValueChange(e);
+})
+slider.addEventListener("touchend", (e) => {
+    sliderFillUpdate();
 })
 
 // - 10K pageviews / $8 per month
